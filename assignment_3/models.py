51bae95091b46752914d17b25a0e1c9be3214f3a
Leyza Yortanlı <lyortanli@gmail.com>
2018-04-16 22:17:13 +0300

    improve models

--- assignment_3/models.py
+++ assignment_3/models.py
@@ -9,20 +9,46 @@ class Sense:
         self.words = []
         self.context = [context]
         self.stemmer = stemmer.PorterStemmer()
+        self.feature = []
+        self.numberofwords = 0
+        
 
     def vectorizeContext(self,context):
         [left, _, right] = re.split(constants.HEAD, context)
-        left_iter = re.finditer(constants.POS, left)
-        right_iter = re.finditer(constants.POS, right)
-        F1_l, F2_l = self.removeStopWords(left_iter)
-        F1_r, F2_r = self.removeStopWords(right_iter)
-        self.getWindow(F1_l, F2_l, right=False)
-        self.getWindow(F1_r, F2_r, right=True)
-        self.words = Counter(self.words)
+        lf1, lf2 = self.extractFeature(re.finditer(constants.POS, left))
+        rf1, rf2 = self.extractFeature(re.finditer(constants.POS, right))
+        self.getWindow(lf1, lf2, False)
+        self.getWindow(rf1, rf2, True)
+
+
+    def getWindow(self, F1, F2, right):
+        if right:
+            bound = 3 if len(F1) >= 3 else len(F1)
+            for i in range(bound):
+                if F1[i] not in constants.STOP_WORDS:
+                    self.feature.append(F1[i])
+                    self.feature.append('{pos}{i}'.format(pos=F2[i], i=i+1))
+        else:
+            bound = len(F1) - 4 if len(F1) >= 3 else -1
+            position = -1
+            for i in range(len(F1)-1, bound, -1):
+                if F1[i] not in constants.STOP_WORDS:
+                    self.feature.append(F1[i])
+                    self.feature.append('{pos}{i}'.format(pos=F2[i], i=position))
+                position -= 1
+
+    def extractFeature(self, iterable):
+        F1 = []
+        F2 = []
+        for iter in iterable:
+            self.numberofwords += 1
+            F1.append(iter.group(1).strip())
+            F2.append(iter.group(2))
+        return F1, F2
 
     def train(self):
         for context in self.context:
-
+            self.vectorizeContext(context)
 
 class Lexelt:
     def __init__(self, name, instances):
@@ -47,8 +73,10 @@ class Lexelt:
 
     def _train(self):
         self._extractInstance()
-        for sense, value in self.senses.items():
-            value.train()
+        for sense_id, sense in self.senses.items():
+            sense.train()
+
+            
 class Model:
 
     def __init__(self, trainpos, testpos):
